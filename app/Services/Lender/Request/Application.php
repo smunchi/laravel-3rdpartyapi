80176af68e27ccd5571954852faf6e877f49e04c
Md. Salahuddin <salah_cse_mbstu@yahoo.com>
2019-09-17 17:28:24 +0600

    update with minor fix

--- app/Services/Lender/Request/Application.php
+++ app/Services/Lender/Request/Application.php
@@ -4,40 +4,17 @@ namespace App\Services\Lender\Request;
 
 class Application
 {
-    public $introducerReference;
-    public $subAffiliate;
-    public $applicationReference;
-    public $isSpeculative;
-    public $isInteractive;
-    public $hasConsentedToCreditSearch;
-    public $hasConsentedForDataSharing;
     public $loanAmount;
     public $loanTerm;
-    public $loanRepaymentFrequency;
-    public $isSecured;
     public $purpose;
-    public $product;
-    public $campaign;
-    public $isPackaged;
-    public $customData;
-    public $repaymentDay;
-    public $repaymentDate;
 
     public function __construct(
         $loanAmount,
         $loanTerm,
-        $purpose,
-        $isSpeculative,
-        $isInteractive,
-        $hasConsentedToCreditSearch,
-        $hasConsentedForDataSharing
+        $purpose
     ) {
         $this->loanAmount = $loanAmount;
         $this->loanTerm = $loanTerm;
         $this->purpose = $purpose;
-        $this->isSpeculative = $isSpeculative;
-        $this->isInteractive = $isInteractive;
-        $this->hasConsentedToCreditSearch = $hasConsentedToCreditSearch;
-        $this->hasConsentedForDataSharing = $hasConsentedForDataSharing;
     }
 }
--- app/Services/Lender/Request/Bank.php
+++ app/Services/Lender/Request/Bank.php
@@ -4,15 +4,10 @@ namespace App\Services\Lender\Request;
 
 class Bank
 {
-    public $accountName;
     public $accountNumber;
     public $sortCode;
-    public $bankName;
-    public $branchName;
     public $yearsAtBank;
     public $monthsAtBank;
-    public $supportsDirectDebit;
-    public $isCurrentAccount;
 
     public function __construct(
         $accountNumber,
